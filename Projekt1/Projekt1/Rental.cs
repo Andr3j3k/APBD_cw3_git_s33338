e62be3b15e5220a06a37f6a9fceaba76ea7dc7d3
Andrzej Pawlak <s33338@pjwstk.edu.pl>
2026-03-22 12:49:22 +0100

    Add Rental model

--- Projekt1/Projekt1/Rental.cs
+++ Projekt1/Projekt1/Rental.cs
@@ -2,5 +2,22 @@
 
 public class Rental
 {
-    
+    public int Id { get; set; }
+    public User User { get; set; }
+    public Equipment Equipment { get; set; }
+    public DateTime BorrowDate { get; set; }
+    public DateTime DueDate { get; set; }
+    public DateTime? ReturnDate { get; set; }
+    public decimal Penalty { get; set; }
+
+    public Rental(int id, User user, Equipment equipment, DateTime borrowDate, DateTime dueDate)
+    {
+        Id = id;
+        User = user;
+        Equipment = equipment;
+        BorrowDate = borrowDate;
+        DueDate = dueDate;
+        ReturnDate = null;
+        Penalty = 0;
+    }
 }
